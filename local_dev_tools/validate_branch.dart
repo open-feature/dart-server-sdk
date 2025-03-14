4c7b535beb9253bf314d425f1b227e2a6edf69a8
ABC2015 <6826984+ABC2015@users.noreply.github.com>
2025-03-14 11:11:43 -0400

    feat: release v.0.0.1-pre+1 (#25)
    Production Push (Pre-release)
    
    - implement core features and unit tests
    
    This is the first pre release version of the Dart SDK.
    
    ---------
    
    Signed-off-by: Jeremy Andrews <jandrews@venturseed.com>
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>
    Signed-off-by: Brian Chebon <brianlchebon@gmail.com>
    Signed-off-by: Usman <ubabar@venturseed.com>
    Signed-off-by: ubabar123 <ubabar@venturseed.com>
    Co-authored-by: Michael Beemer <beeme1mr@users.noreply.github.com>
    Co-authored-by: Brian Chebon <brianlchebon@gmail.com>
    Co-authored-by: ubabar123 <ubabar@venturseed.com>

--- local_dev_tools/validate_branch.dart
+++ local_dev_tools/validate_branch.dart
@@ -14,61 +14,21 @@ void main(List<String> args) {
 
   print('Branch name: $branchName');
 
-<<<<<<< HEAD
   final validBranches = RegExp(r'^(qa|development|main)$');
   final validFeatureBranch = RegExp(
     r'^(feat|fix|hotfix|chore|test|refactor|release)/[a-z0-9_-]+$',
   );
-=======
-  final validBranches = RegExp(r'^(qa|beta|main)$');
-<<<<<<< HEAD
-<<<<<<< HEAD
-  final validFeatureBranch =
-      RegExp(r'^(feat|fix|hotfix|chore|test|refactor|release)/[a-z0-9_-]+$');
->>>>>>> 83b6438 (feat: release v.0.0.1-pre+1 (#25))
-=======
-  final validFeatureBranch = RegExp(
-    r'^(feat|fix|hotfix|chore|test|refactor|release)/[a-z0-9_-]+$',
-  );
->>>>>>> 5eb4a58 (chore: update validation workflow)
-=======
-  final validFeatureBranch = RegExp(
-    r'^(feat|fix|hotfix|chore|test|refactor|release)/[a-z0-9_-]+$',
-  );
->>>>>>> 9729726 (chore: update dartsdk minimum to latest version 3.7.2 (#44))
 
   if (validBranches.hasMatch(branchName) ||
       validFeatureBranch.hasMatch(branchName)) {
     print('✅ Branch name is valid.');
   } else {
     print(
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
       '❌ Branch name does not follow the required convention: <type>/<branch-name>',
     );
     print(
       'Valid types: feat, fix, hotfix, chore, test, refactor, release, qa, development, main',
     );
-=======
-        '❌ Branch name does not follow the required convention: <type>/<branch-name>');
-    print(
-        'Valid types: feat, fix, hotfix, chore, test, refactor, release, qa, beta, main');
->>>>>>> 83b6438 (feat: release v.0.0.1-pre+1 (#25))
-=======
-      '❌ Branch name does not follow the required convention: <type>/<branch-name>',
-    );
-    print(
-      'Valid types: feat, fix, hotfix, chore, test, refactor, release, development, qa, main',
-    );
->>>>>>> 5eb4a58 (chore: update validation workflow)
-=======
-      '❌ Branch name does not follow the required convention: <type>/<branch-name>',
-    );
-    print(
-      'Valid types: feat, fix, hotfix, chore, test, refactor, release, development, qa, main',
-    );
->>>>>>> 9729726 (chore: update dartsdk minimum to latest version 3.7.2 (#44))
     exit(1);
   }
 }
