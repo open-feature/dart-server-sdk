1781fdcf198a506b55d50972d69ffaaf0d4b7baf
Brian Chebon <brianlchebon@gmail.com>
2025-12-25 18:13:20 +0300

    chore: updated test files (#85)
    ## This PR
    
    - Enhanced testability - Added test mode factory constructors to
    OpenFeatureAPI
    - Eliminated duplication - Removed duplicate test groups and code
    comments
    - Maintained spec compliance - All OpenFeature specification
    requirements intact
    
    Signed-off-by: Brian Chebon <brianlchebon@gmail.com>

--- test/client_test.dart
+++ test/client_test.dart
@@ -134,140 +134,6 @@ class MockProvider implements FeatureProvider {
     throw UnimplementedError();
   }
 }
-import '../lib/feature_provider.dart';
-import '../lib/evaluation_context.dart';
-import '../lib/hooks.dart';
-
-class MockProvider implements FeatureProvider {
-  final Map<String, dynamic> flags;
-  ProviderState _state = ProviderState.READY;
-
-  MockProvider(this.flags);
-
-  @override
-  String get name => 'MockProvider';
-
-  @override
-  ProviderState get state => _state;
-
-  @override
-  ProviderConfig get config => ProviderConfig();
-
-  @override
-  ProviderMetadata get metadata => ProviderMetadata(name: 'MockProvider');
-
-  @override
-  Future<void> initialize([Map<String, dynamic>? config]) async {
-    _state = ProviderState.READY;
-  }
-
-  @override
-  Future<void> connect() async {}
-
-  @override
-  Future<void> shutdown() async {
-    _state = ProviderState.SHUTDOWN;
-  }
-
-  @override
-  Future<FlagEvaluationResult<bool>> getBooleanFlag(
-    String flagKey,
-    bool defaultValue, {
-    Map<String, dynamic>? context,
-  }) async {
-    if (!flags.containsKey(flagKey)) {
-      return FlagEvaluationResult.error(
-        flagKey,
-        defaultValue,
-        ErrorCode.FLAG_NOT_FOUND,
-        'Flag not found',
-        evaluatorId: name,
-      );
-    }
-
-    final value = flags[flagKey];
-    if (value is! bool) {
-      return FlagEvaluationResult.error(
-        flagKey,
-        defaultValue,
-        ErrorCode.TYPE_MISMATCH,
-        'Type mismatch',
-        evaluatorId: name,
-      );
-    }
-
-    return FlagEvaluationResult(
-      flagKey: flagKey,
-      value: value,
-      reason: 'STATIC',
-      evaluatedAt: DateTime.now(),
-      evaluatorId: name,
-    );
-  }
-
-  @override
-  Future<FlagEvaluationResult<String>> getStringFlag(
-    String flagKey,
-    String defaultValue, {
-    Map<String, dynamic>? context,
-  }) async {
-    if (!flags.containsKey(flagKey)) {
-      return FlagEvaluationResult.error(
-        flagKey,
-        defaultValue,
-        ErrorCode.FLAG_NOT_FOUND,
-        'Flag not found',
-        evaluatorId: name,
-      );
-    }
-
-    final value = flags[flagKey];
-    if (value is! String) {
-      return FlagEvaluationResult.error(
-        flagKey,
-        defaultValue,
-        ErrorCode.TYPE_MISMATCH,
-        'Type mismatch',
-        evaluatorId: name,
-      );
-    }
-
-    return FlagEvaluationResult(
-      flagKey: flagKey,
-      value: value,
-      reason: 'STATIC',
-      evaluatedAt: DateTime.now(),
-      evaluatorId: name,
-    );
-  }
-
-  @override
-  Future<FlagEvaluationResult<int>> getIntegerFlag(
-    String flagKey,
-    int defaultValue, {
-    Map<String, dynamic>? context,
-  }) async {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<FlagEvaluationResult<double>> getDoubleFlag(
-    String flagKey,
-    double defaultValue, {
-    Map<String, dynamic>? context,
-  }) async {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<FlagEvaluationResult<Map<String, dynamic>>> getObjectFlag(
-    String flagKey,
-    Map<String, dynamic> defaultValue, {
-    Map<String, dynamic>? context,
-  }) async {
-    throw UnimplementedError();
-  }
-}
 
 void main() {
   group('ClientMetadata Tests', () {
@@ -292,13 +158,12 @@ void main() {
 
   group('ClientMetrics Tests', () {
     test('calculates average response time', () {
-      final metrics =
-          ClientMetrics()
-            ..responseTimes.addAll([
-              Duration(milliseconds: 100),
-              Duration(milliseconds: 200),
-              Duration(milliseconds: 300),
-            ]);
+      final metrics = ClientMetrics()
+        ..responseTimes.addAll([
+          Duration(milliseconds: 100),
+          Duration(milliseconds: 200),
+          Duration(milliseconds: 300),
+        ]);
 
       expect(metrics.averageResponseTime, equals(Duration(milliseconds: 200)));
     });
@@ -317,12 +182,10 @@ void main() {
     });
 
     test('converts to JSON correctly', () {
-
       final metrics = ClientMetrics()
         ..flagEvaluations = 10
         ..responseTimes.add(Duration(milliseconds: 100))
         ..errorCounts['TestError'] = 1;
-
 
       final json = metrics.toJson();
 
@@ -380,7 +243,6 @@ void main() {
       expect(metrics.flagEvaluations, equals(2));
       expect(metrics.errorCounts['FLAG_NOT_FOUND'], equals(1));
     });
-
 
     test('evaluates string flags', () async {
       final result = await client.getStringFlag('string-flag');
